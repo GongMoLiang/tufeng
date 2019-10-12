3b49ec1e98dd50743e5b7bdb383537f38dd3d464
GongMoLiang <gml18720826651@163.comm>
2019-10-12 17:46:51 +0800

    个人中心页面

--- src/views/home/center.vue
+++ src/views/home/center.vue
@@ -1,3 +1,32 @@
 <template>
-    <div>这是个人中心页面</div>
+  <div class="page-center">
+    <div class="login">
+      <!-- 头像图片 -->
+      <div class="head">
+        <img src="https://m.toursforfun.com/app/mytff" alt />
+      </div>
+      <div class="choice">
+        <span>登入</span>/
+        <span>注册</span>
+      </div>
+      <ul>
+        <li>
+          <span>0</span>
+          <span>积分</span>
+        </li>
+        <li>
+          <span>0</span>
+          <span>收藏</span>
+        </li>
+        <li>
+          <span>0</span>
+          <span>收藏</span>
+        </li>
+        <li>
+          <span>0</span>
+          <span>浏览历史</span>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
