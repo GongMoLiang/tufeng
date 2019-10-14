e72d2dfe1c973f95d0376f3df6283ded77aaf73f
mewtwo <641383539@qq.com>
2019-10-14 10:02:20 +0800

    test

--- src/views/home/dest.vue
+++ src/views/home/dest.vue
@@ -1,3 +1,40 @@
 <template>
-    <div>这是目的地页</div>
+  <div class="dest-header">
+    <div class="bg">
+      <img src="../../assets/images/usa.jpg" alt />
+    </div>
+    <a href="#">
+      <i class="iconfont icon-sousuo"></i>
+    </a>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Dest'
+}
+</script>
+
+
+<style lang="scss">
+.dest-header {
+  width: 100%;
+  height: 60vh;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: rgba(0, 0, 0, 0.1);
+
+  a {
+    position: absolute;
+    right: 18px;
+    top: 24px;
+    color: #fff;
+  }
+
+  .bg img {
+    width: 100%;
+    height: 441px;
+  }
+}
+</style>
